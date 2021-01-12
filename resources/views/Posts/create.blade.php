9a301e4fcd62659fabeeed313635e4d6c5c51ed8
Yehia El Sayed <yehiafml20333@gmail.com>
2021-01-12 14:50:05 +0200

    Using forms collective package

--- resources/views/Posts/create.blade.php
+++ resources/views/Posts/create.blade.php
@@ -2,10 +2,12 @@
 
 
 
-    <form method="post" action="/posts">
+{{--    <form method="post" action="/posts">--}}
+
+{!! Form::open(['method'=>'POST', 'action'=>'App\Http\Controllers\PostsController@store']) !!}
 
         <input type="text" name="title" placeholder="Enter the title">
-        {{@csrf_field()}}
+{{--        {{@csrf_field()}}--}}
         <input type="text" name="content" placeholder="Enter the content">
         <input type="submit" name="submit">
 
